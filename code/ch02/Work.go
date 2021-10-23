f44e59ee21c8367c3d6e30a8d9cb39dcc1f8937a
oF <sky0932039612@gmail.com>
2021-10-23 18:16:31 +0800

    增加備註

--- code/ch02/Work.go
+++ code/ch02/Work.go
@@ -7,6 +7,6 @@ func main() {
 	const distance = 56000000
 	var day = 28
 	day *= 24 // day to hour
-	var speed = distance / day
+	var speed = distance / day // calculate the speed
 	fmt.Print("時速 ", speed + 1, " km/h，即可於 28 天內到達 Malacandra")
 }
