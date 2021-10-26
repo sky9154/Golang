033edc9cd696dd3ffcbfd8f6fe8f8c759e5587c1
oF <sky0932039612@gmail.com>
2021-10-26 20:49:30 +0800

    新增註解

--- code/ch05/Work.go
+++ code/ch05/Work.go
@@ -35,34 +35,35 @@ import (
 func ticket(seed int64) (day int) {
 	rand.Seed(seed)
 	speed := rand.Intn(46) + 16	// 隨機速度
-	day = int (math.Ceil(float64 (62100000 / (speed * 60 * 60 * 24))))
+	day = int (math.Ceil(float64 (62100000 / (speed * 60 * 60 * 24))))	// 無條件進位
 	return
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano()) // 隨機種子
+	rand.Seed(time.Now().UnixNano())	// 隨機種子
 
 	Spaceline := [3] string {	// Spaceline 航空公司
 		"Space Adventures", 
 		"SpaceX", 
 		"Virgun Galactic",
 	}
-	TripType := [2] string {	// 隨機決定單程或往返
+	TripType := [2] string {	// 單程或往返
 		"Round - trip",
 		"One - way",
 	}
 
 	fmt.Printf("%-20s%-10s%-15s%-10s\n","Spaceline", "Days", "Trip Type", "Price")
 	fmt.Println("==================================================")
+	
 	for i := 0; i < 9; i ++ {
 		day := ticket(int64(i))
-		TripTypeRand := TripType[rand.Intn(2)]
-		price := 0
-		if TripTypeRand == "One - way" {
-			price = rand.Intn(14) +36
-		} else {
-			price = (rand.Intn(14) +36) * 2
+		TripTypeRand := TripType[rand.Intn(2)]	// 隨機決定單程或往返
+		price := rand.Intn(14) +36	// 隨機票價
+
+		if TripTypeRand == "Round - trip" {	// 判斷是否需往返，如果成立則車票錢 * 2
+			price *= 2
 		}
+		
 		fmt.Printf("%-20s%-10d%-15s%-10d\n", Spaceline[rand.Intn(3)], day, TripTypeRand, price)
 	}
 }
